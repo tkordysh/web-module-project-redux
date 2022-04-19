7488f2f94955ff4b4a6b117569781f12f6334855
Tony Kordysh <tkordysh@gmail.com>
2022-04-19 16:03:18 -0500

    initial push

--- src/components/App.js
+++ src/components/App.js
@@ -17,7 +17,7 @@ const App = props => {
   return (
     <div>
       <nav className="navbar navbar-dark bg-dark">
-        <span className="navbar-brand" >Redux Module Project</span>
+        <span className="navbar-brand" >TK Redux Module Project</span>
       </nav>
 
       <div className="container">
